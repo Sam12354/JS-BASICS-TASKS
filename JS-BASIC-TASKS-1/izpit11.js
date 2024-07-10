7ad988508ddad053fdb69d7ee7ba86286d6cc1e9
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-10 13:12:38 +0300

    TASK-07

---
+++ JS-BASIC-TASKS-1/izpit11.js
@@ -0,0 +1,11 @@
+function triangleOfNumbers(maxRow) {
+
+    for (let num = 1; num <= maxRow; num++) {
+        console.log(`${num} `.repeat(num));
+    }
+
+}
+triangleOfNumbers
+(3)
+// (5)
+// (6)
