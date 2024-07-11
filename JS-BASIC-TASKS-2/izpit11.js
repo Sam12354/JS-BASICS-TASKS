d8d2b45b942ee26b55a7d9c62e5f3b287ef6f08d
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-11 11:21:27 +0300

    TASK-03

---
+++ JS-BASIC-TASKS-2/izpit11.js
@@ -0,0 +1,9 @@
+function townInfo(city, population, area){
+    
+    console.log(`Town ${city} has population of ${population} and area ${area} square km.`);
+
+}
+townInfo
+('Sofia', 1286383, 492)
+
+// ('Plovdiv', 1481353, 512)
