86eed98673cfae5a1d8858c52bf4ebb8f9dbd501
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-11 11:36:31 +0300

    TASK-09-FOR-LOOP.js

--- JS-BASIC-TASKS-2/izpit11.js
+++ JS-BASIC-TASKS-2/izpit11.js
@@ -1,11 +1,38 @@
-function upperOrLowerCase(char){
-    if(char === char.toUpperCase()){
-        console.log('upper-case');
-    }else{
-        console.log('lower-case');
-    }
-}
-upperOrLowerCase
-('L')
+function primeOrNonPrime(input){
+    let command = input[0];
+    let index = 1;
+    let primeSum = 0;
+    let primeNonSum = 0;
 
-// ('f')
+    while(command !== "stop"){
+        let num = Number(command);
+        let isPrime = true;
+
+        if(num < 0){
+            console.log("Number is negative.");
+            command = input[index];
+            index++;
+            continue;
+        }
+        
+        for(let divisor = 2; divisor < num; divisor++){
+            
+            if(num % divisor === 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            primeSum += num;
+        }else {
+            primeNonSum += num;
+        }
+        command = input[index];
+        index++;
+    }
+    console.log(`Sum of all prime numbers is: ${primeSum}`);
+    console.log(`Sum of all non prime numbers is: ${primeNonSum}`);
+}
+primeOrNonPrime
+(["3", "9", "0", "7", "19", "4", "stop"]);
+//(["30", "83", "33", "-1", "20", "stop"]);
