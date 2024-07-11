355e4723b0f774a35f0d6b1e5dfc8f6f0e2bc0b2
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-11 11:25:50 +0300

    TASK-05

---
+++ JS-BASIC-TASKS-2/izpit11.js
@@ -0,0 +1,15 @@
+// Write a function that converts British pounds to dollars formatted to the 3rd decimal point.
+
+// 1 British Pound = 1.31 Dollars
+
+function moneyConverter(num){
+    
+    let pound = 1.31
+    let result = num * pound;
+    console.log(result.toFixed(3));
+
+}
+moneyConverter
+(80)
+
+// (39)
