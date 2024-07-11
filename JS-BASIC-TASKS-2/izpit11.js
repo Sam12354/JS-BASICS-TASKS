ed1f61e8f9d20fe1f9e3aa6d46a538a398637cd0
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-11 11:23:27 +0300

    TASK-04

---
+++ JS-BASIC-TASKS-2/izpit11.js
@@ -0,0 +1,14 @@
+// You will be given a number that will be the distance in meters.
+// Write a program that converts meters to kilometers formatted to the second decimal point.
+
+function converter(num){
+    
+    let km = 1000
+    let result = num / km;
+    console.log(result.toFixed(2));
+
+}
+converter
+(1852)
+
+// (798)
