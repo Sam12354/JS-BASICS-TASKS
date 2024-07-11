23f4f1db6be747cbd87a1f898bb520965e8d8154
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-11 11:18:30 +0300

    TASK-02

---
+++ JS-BASIC-TASKS-2/izpit11.js
@@ -0,0 +1,24 @@
+function charToString(char1, char2, char3){
+    
+    let sum = char1 + char2 + char3;
+    console.log(sum);
+
+}
+charToString
+('a',
+
+'b',
+
+'c' )
+
+// ('%',
+
+// '2',
+
+// 'o' )
+
+// ('1',
+
+// '5',
+
+// 'p' )
