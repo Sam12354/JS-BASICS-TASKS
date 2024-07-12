16134e12025a3787a4fc23b0586e34b713116d97
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-12 11:49:48 +0300

    Update and rename izpit1.js to TASK-05.js

---
+++ JS-OBJECTS/TASK-05.js
@@ -0,0 +1,31 @@
+function inventory(array){
+    let curHero = {};
+    let allHeroes = [];
+ 
+    for(let heroGame of array){
+        let commands = heroGame.split(' / ');
+        let hero = commands[0];
+        let level = Number(commands[1]);
+        let items = commands[2];
+ 
+        curHero = { hero, level, items }
+        allHeroes.push(curHero)
+    }
+ 
+    allHeroes.sort((a, b) => a.level - b.level);
+    for(let hero of allHeroes){
+        console.log(`Hero: ${hero.hero}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items}`);
+    }
+}
+inventory
+([
+
+    'Isacc / 25 / Apple, GravityGun',
+    
+    'Derek / 12 / BarrelVest, DestructionSword',
+    
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    
+])
