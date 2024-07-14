78c927ae234cea2269e25dc6059b60dc9e356c78
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-14 12:50:37 +0300

    task5-01

---
+++ JS-BASIC-TASKS-3/05-MIDEXAM-ZAD-1.js
@@ -0,0 +1,32 @@
+function zad3(array){
+    let numberOfStudents = Number(array.shift());
+    let totalLectures = Number(array.shift());
+    let auditionalBonus = Number(array.shift());
+
+    let studentMaxBonus = 0;
+    let totalBonus = 0;
+    let highAttendance = 0;
+
+    for(let i = 0; i < numberOfStudents; i++){
+        let attendances = Number(array[i]);
+
+        totalBonus = attendances / totalLectures * (5 + auditionalBonus);
+
+        if(totalBonus > studentMaxBonus){
+            studentMaxBonus = totalBonus
+        }
+        if(attendances > highAttendance){
+            highAttendance = attendances
+        }
+
+
+    }
+    console.log(`Max Bonus: ${Math.ceil(studentMaxBonus)}.`);
+    console.log(`The student has attended ${highAttendance} lectures.`);
+}
+zad3
+([
+    '5',  '25', '30',
+    '12', '19', '24',
+    '16', '20'
+])
