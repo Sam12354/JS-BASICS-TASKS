13839781dcc6e4232cb1e235b4c6484077e17ce2
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-14 12:57:13 +0300

    task06-01

---
+++ JS-BASIC-TASKS-3/06-MIDEXAM-ZAD-1.js
@@ -0,0 +1,33 @@
+function zad1(array){
+    let daysLast = Number(array.shift());
+    let dayPludner = Number(array.shift());
+    let expectedPlunder = Number(array.shift());
+    let day = 1;
+    let totalPlunder = 0;
+    let additional = 0;
+
+    for(let i = 0; i < daysLast; i++){
+        totalPlunder += dayPludner
+        if(day % 3 === 0){
+            additional = dayPludner * 0.5
+            totalPlunder += additional
+        }
+        if(day % 5 === 0){
+            totalPlunder *= 0.7
+        }
+        day++
+    }
+
+
+    if(totalPlunder >= expectedPlunder){
+        console.log(`Ahoy! ${totalPlunder.toFixed(2)} plunder gained.`);
+    }else{
+        let percent = (totalPlunder / expectedPlunder) * 100
+        console.log(`Collected only ${percent.toFixed(2)}% of the plunder.`);
+    }
+
+}
+zad1
+(["10",
+"20",
+"380"])
