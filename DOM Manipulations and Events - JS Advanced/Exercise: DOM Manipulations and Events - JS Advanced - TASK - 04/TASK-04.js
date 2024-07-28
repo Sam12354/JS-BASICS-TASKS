d42bb4660797cf85ac92b510ec697fe1558fcdd8
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-28 22:36:49 +0300

    Update and rename dropdown.js to TASK-04.js

--- DOM Manipulations and Events - JS Advanced/Exercise: DOM Manipulations and Events - JS Advanced - TASK - 04/TASK-04.js
+++ DOM Manipulations and Events - JS Advanced/Exercise: DOM Manipulations and Events - JS Advanced - TASK - 04/TASK-04.js
@@ -5,19 +5,11 @@ function addItem() {
     const getSelectMenu = document.getElementById("menu");
     const createOption = document.createElement("option");
 
-    // set textContent and value
     createOption.textContent = getNewItemText;
-    //"set its textContent to the newly taken ones."
+
     createOption.value = getNewItemValue;
-    //"set its value to the newly taken ones."
 
     getSelectMenu.append(createOption);
-
-    // const clearTextInput = getNewItemText.value = ''
-    // const clearValueInput = getNewItemValue.value = ''
-
-    // You need to clear the input fields, 
-    // but getNewItemText and getNewItemValue are strings, not input elements.
 
     const clearTextInput = document.getElementById("newItemText").value = '';
     const clearValueInput = document.getElementById("newItemValue").value = '';
