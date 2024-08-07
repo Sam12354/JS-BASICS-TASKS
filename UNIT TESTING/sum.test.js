9d6da08780a19fff860f6a38c175eeb7350af04c
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-08-07 13:25:48 +0300

    Add files via upload

---
+++ UNIT TESTING/sum.test.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { sum } from "../sum.js"; 
+
+describe('Sum', () => {
+    it('Should return the sum of numbers', () => {
+        const input = [1, 2, 3];
+        const expectedResult = 6;
+
+        const actualResult = sum(input);
+        expect(actualResult).to.equal(expectedResult);
+    });
+});
