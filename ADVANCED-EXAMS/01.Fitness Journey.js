1cb06c3648e3e9cfbde230fd686a8c25db3e86b1
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-08-29 12:52:45 +0300

    Update 01.Fitness Journey.js

--- ADVANCED-EXAMS/01.Fitness Journey.js
+++ ADVANCED-EXAMS/01.Fitness Journey.js
@@ -159,4 +159,34 @@ function solve() {
 
 
 
+// Write the missing functionality of this user interface. The functionality is divided in the following steps: 
 
+// Write the missing JavaScript code to make the Fitness Journey work as expected:
+// All fields (Name, Email, Contact Number, Preferred Class and Class Time) are filled with the correct input.
+// •	Name, Email, Contact Number, Preferred Class and Class Time are non-empty strings. If any of them is empty, the program should not do anything.
+
+// 1.	Getting the information from the form
+ 
+// •	On clicking the ["Next"] button the information from the input fields is listed in the "preview-section" section. For the input fields a list item is added to the "class-info" unordered list. 
+// •	The text format and order for the list item should be the same as on the picture below.  
+// •	When the button is clicked, the input fields must be cleared and the ["Next"] button must be disabled. At the same time the "Edit" and the "Continue" buttons must be added. 
+// The HTML structure looks like this:  
+ 
+
+// The functionality here is the following: 
+// •	When the "Edit" button is clicked, all of the information is loaded in the input fields from step 1 and the ["Next"] button is enabled again.
+ 
+// •	The list item must be removed from the "class-info" and all of the information must go back to the input fields again. 
+ 
+// •	When the "Continue" button is clicked, the information from "class-info" unordered list must be transferred to "confirm-class" in the same HTML structure. For you, this means removing everything inside of the ul with class  "class-info" and adding in "confirm-class", the list item with same information and class "continue-info"   , delete "Edit" and "Continue" buttons and add new buttons "Confirm" and "Cancel" with class "confirm-btn" and "cancel-btn" .
+ 
+
+// •	This is the HTML structure of "confirm-class" unordered list:
+ 
+// •	When the "Cancel" button is clicked, the list item must be removed, from the "confirm-class", the ["Next"] button is enabled again.
+ 
+
+// •	When the "Confirm" button is clicked, the <div> element with id="main" must be removed, from the body and  you must add <h1> element with id="thank-you" and text "Thank you for scheduling your appointment, we look forward to seeing you!" and  <button> with id="done-btn" and text "Done".
+ 
+
+// •	On clicking the ["Done"] button you must reload the page.
