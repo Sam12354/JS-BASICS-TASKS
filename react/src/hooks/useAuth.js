68da17ed3b47c9778d5054d4d2afe83dafa54c88
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2025-02-17 19:22:11 +0200

    Add files via upload

---
+++ react/src/hooks/useAuth.js
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { login, register } from "../api/auth-api.js";
+import { AuthContext } from "../contexts/AuthContext.js";
+
+export const useLogin = () => {
+
+    const { changeAuthState } = useContext(AuthContext)
+
+    const loginHandler = async (email, password) => {
+        
+        const result = await login(email, password)
+        changeAuthState(result)
+    }
+
+    return loginHandler
+}
+
+export const useRegister = () => {
+
+    const { changeAuthState } = useContext(AuthContext)
+
+    const registerHandler = async (email, password) => {
+        
+        const result = await register(email, password)
+        changeAuthState(result)
+    }
+
+    return registerHandler
+}
+
